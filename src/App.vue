13e042fdea70c45af39dcb7c29c71ca232ab70b9
Gabo <gab_org@hotmail.com>
2020-03-09 11:46:49 -0600

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <MessageList :messages="messages"/>
+    <Form/>
+  </div>
+</template>
+
+<script>
+import MessageList from './components/MessageList'
+import Form from './components/Form'
+
+export default {
+  name: 'app',
+  data: () => ({ messages: ['Hey John', 'Howdy Paco'] }),
+  components: {
+    MessageList,
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form action @submit.prevent="onSubmit(inputValue)">
+      <input type="text" v-model="inputValue">
+      <span class="reversed">{{ reversedInput }}</span>
+
+      <button type="submit">Send</button>
+    </form>
+
+    <br>
+    <p v-for="res in results" class="result">{{ res.title }}</p>
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    inputValue: "",
+    results: []
+  }),
+  props: ["reversed"],
+  computed: {
+    reversedInput() {
+      return this.reversed
+        ? this.inputValue
+            .split("")
+            .reverse()
+            .join("")
+        : this.inputValue;
+    }
+  },
+  methods: {
+    onSubmit(value) {
+      const getPromise = axios.get(
+        "https://jsonplaceholder.typicode.com/posts?q=" + value
+      );
+      console.log(axios);
+      getPromise.then(results => {
+        this.results = results.data;
+      });
+      console.log("this.results", this.results);
+      console.log("getPromise", getPromise);
+      return getPromise;
+    }
+  },
+  watch: {
+    inputValue(newVal, oldVal) {
+      //console.log(newVal, oldVal);
+      if (newVal.trim().length && newVal !== oldVal) {
+        console.log(newVal);
+      }
+    }
+  }
+};
+</script>
